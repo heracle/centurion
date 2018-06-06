0bf5d77ec9ba66874d04b0f4708b049d1adf4302
Radu <mcradu9@gmail.com>
2018-06-06 21:55:04 +0300

    add usa

--- strategies/russia.py
+++ strategies/russia.py
@@ -1,13 +1,9 @@
 def strategy(controller, round):
-  if round == 15:
-    controller.move({'x':7,'y': 3})
+  
 
-  if round % 4 == 0:
-    controller.shot({'x': 1, 'y': 2})
-  if round % 4 == 2:
-    controller.shot({'x': 1, 'y': 3})
-
-  if round % 4 == 1:
-    controller.move({'x':8, 'y':3})
-  if round % 4 == 3:
-    controller.move({'x':8, 'y':2})
+  if round % 2 == 0:
+    controller.shot({'x':8, 'y':2})
+    controller.shot({'x':8, 'y':3})
+    
+  controller.move({'x':1, 'y':2})
+  controller.move({'x':1, 'y':3})
