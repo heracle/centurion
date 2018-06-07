10d72437848f6d5eb6ccfb400ce8ee9fe0f40b72
Radu <mcradu9@gmail.com>
2018-06-07 13:32:25 +0300

    integrate bosslang

--- src/bosslang/lib/stdmethods.py
+++ src/bosslang/lib/stdmethods.py
@@ -4,15 +4,62 @@
 class Injector():
     bosslang_injected_methods = None
 
-def isgroundcell(x, y):
+def getTeam():
+	return Injector.bosslang_injected_methods.get_team()
+
+def getCoordinatesX():
+	return Injector.bosslang_injected_methods.get_coordinates_x()
+
+def getCoordinatesY():
+	return Injector.bosslang_injected_methods.get_coordinates_y()
+
+def getTankHpByIndex(index):
+	return Injector.bosslang_injected_methods.get_tankhp_by_index(index)
+
+def getTotalNumberTanks():
+	return Injector.bosslang_injected_methods.get_total_number_tanks()
+
+def getTankTeam(index):
+	return Injector.bosslang_injected_methods.get_tank_team(index)
+
+def getGridSizeX():
+	return Injector.bosslang_injected_methods.get_grid_size_x()
+
+def getGridSizeY():
+	return Injector.bosslang_injected_methods.get_grid_size_y()
+
+def getMyTankCoordinatesX(): #the same as getCoordinatesX
+	return Injector.bosslang_injected_methods.get_my_tank_coordinates_x()
+
+def getMyTankCoordinatesY(): #the same as getCoordinatesY
+	return Injector.bosslang_injected_methods.get_my_tank_coordinates_y()
+
+def getEnemyTankCoordinatesX(index):
+	return Injector.bosslang_injected_methods.get_enemy_tank_coordinates_x(index)
+
+def getEnemyTankCoordinatesY(index):
+	return Injector.bosslang_injected_methods.get_enemy_tank_coordinates_y(index)
+
+def isGroundCell(x, y):
     return Injector.bosslang_injected_methods.is_ground_cell(x, y)
 
-def getmycoord():
-    r = Injector.bosslang_injected_methods.get_my_tank_coordinates()
-    return r
+def isWaterCell(x, y):
+	return Injector.bosslang_injected_methods.is_water_cell(x, y)
 
-def move():
-    print("Execute a move up command")
+def isObjectCell(x, y):
+	return Injector.bosslang_injected_methods.is_object_cell(x, y)
+
+def isProjectileCell(x, y):
+	return Injector.bosslang_injected_methods.is_projectile_cell(x, y)
+
+def getTypeCell(x, y):
+	return Injector.bosslang_injected_methods.get_type_cell(x, y)
+
+def doMove(x, y):
+	return Injector.bosslang_injected_methods.do_move(x, y)
+
+def doShot(x, y):
+	return Injector.bosslang_injected_methods.do_shot(x, y)
 
 def squared(x):
     return x * x
--- src/bosslangapi.py
+++ src/bosslangapi.py
@@ -4,116 +4,110 @@ class BosslangApi:
     def __init__(self, controller):
         self.controller = controller
 
-    def get_team(self)
-        r = []
-        r.append(self.controller.get_team())
-        return r
+    def get_team(self):
+        return self.controller.get_team()
     
-    def get_coordinates(self)
+    def get_coordinates_x(self):
         coord = self.controller.get_coordinates()
-        r = []
-        r.append(coord['x'])
-        r.append(coord['y'])
-        return r
+        return coord['x']
 
-    def get_tankhp_by_index(self, index)
-        r = []
-        r.append(self.controller.get_tankhp_by_index(index))
-        return r
+    def get_coordinates_y(self):
+        coord = self.controller.get_coordinates()
+        return coord['y']
 
-    def get_total_number_tanks(self)
-        r = []
-        r.append(self.controller.get_total_number_tanks())
-        return r
+    def get_tankhp_by_index(self, index):
+        return self.controller.get_tankhp_by_index(index)
 
-    def get_tank_team(self, index)
-        r = []
-        r.append(self.controller.get_tank_team(index))
-        return r
+    def get_total_number_tanks(self):
+        return self.controller.get_total_number_tanks()
 
-    def get_grid_size(self)
-        r = []
+    def get_tank_team(self, index):
+        return self.controller.get_tank_team(index)
+
+    def get_grid_size_x(self):
         coord = self.controller.get_grid_size()
-        x = coord['x']
-        y = coord['y']
-        r = []
-        r.append(x)
-        r.append(y)
-        return r
+        return coord['x']
 
-    def get_my_tank_coordinates(self):
+    def get_grid_size_y(self):
+        coord = self.controller.get_grid_size()
+        return coord['y']
+
+    def get_my_tank_coordinates_x(self):
         coord = self.controller.get_my_tank_coordinates()
-        x = coord['x']
-        y = coord['y']
-        r = []
-        r.append(x)
-        r.append(y)
-        return r
+        return coord['x']
     
-    def get_enemy_tank_coordinates(self, enemy_id):
+    def get_my_tank_coordinates_y(self):
+        coord = self.controller.get_my_tank_coordinates()
+        return coord['y']
+
+    def get_enemy_tank_coordinates_x(self, enemy_id):
         coord = self.controller.get_enemy_tank_coordinates(enemy_id)
-        x = coord['x']
-        y = coord['y']
-        r = []
-        r.append(x)
-        r.append(y)
-        return r
+        return coord['x']
+
+    def get_enemy_tank_coordinates_y(self, enemy_id):
+        coord = self.controller.get_enemy_tank_coordinates(enemy_id)
+        return coord['y']
 
     def is_ground_cell(self, x, y):
-        r = []
-        r.append(self.controller.is_ground_cell({'x': x, 'y':y}))
-        return r
+        return self.controller.is_ground_cell({'x': x, 'y':y})
     
     def is_water_cell(self, x, y):
-        r = []
-        r.append(self.controller.is_water_cell({'x': x, 'y':y}))
-        return r
+        return self.controller.is_water_cell({'x': x, 'y':y})
     
     def is_object_cell(self, x, y):
-        r = []
-        r.append(self.controller.is_object_cell({'x': x, 'y':y}))
-        return r
+        return self.controller.is_object_cell({'x': x, 'y':y})
     
     def is_projectile_cell(self, x, y):
-        r = []
-        r.append(self.controller.is_projectile_cell({'x': x, 'y':y}))
-        return r
+        return self.controller.is_projectile_cell({'x': x, 'y':y})
 
-    def get_water_cells(self):
-        r = []
-        rec = self.controller.get_water_cells()
-        r.append(rec.__len__())
-        for i in range(rec.__len__()):
-            r.append(rec[i]['x'])
-            r.append(rec[i]['y'])
-        return r
+    def get_type_cell(self, x, y):
+        if elf.controller.is_ground_cell({'x': x, 'y':y}) ==  True:
+            return 0
+        if self.controller.is_water_cell({'x': x, 'y':y}) ==  True:
+            return 1
+        if self.controller.is_object_cell({'x': x, 'y':y}) == True:
+            return 2
+        if self.controller.is_projectile_cell({'x': x, 'y':y}) == True:
+            return 3
+        assert False
+        return -1 #hope to be rendumdant
 
-    def get_ground_cells(self)
-        r = []
-        rec = self.controller.get_ground_cells()
-        r.append(rec.__len__())
-        for i in range(rec.__len__()):
-            r.append(rec[i]['x'])
-            r.append(rec[i]['y'])
-        return r
 
-    def get_obstacle_cells(self)
-        r = []
-        rec = self.controller.get_obstacle_cells()
-        r.append(rec.__len__())
-        for i in range(rec.__len__()):
-            r.append(rec[i]['x'])
-            r.append(rec[i]['y'])
-        return r
+    # def get_water_cells(self):
+    #     r = []
+    #     rec = self.controller.get_water_cells()
+    #     r.append(rec.__len__())
+    #     for i in range(rec.__len__()):
+    #         r.append(rec[i]['x'])
+    #         r.append(rec[i]['y'])
+    #     return r
 
-    def get_projectiles(self)
-        r = []
-        rec = self.controller.get_projectiles()
-        r.append(rec.__len__())
-        for i in range(rec.__len__()):
-            r.append(rec[i]['x'])
-            r.append(rec[i]['y'])
-        return r
+    # def get_ground_cells(self):
+    #     r = []
+    #     rec = self.controller.get_ground_cells()
+    #     r.append(rec.__len__())
+    #     for i in range(rec.__len__()):
+    #         r.append(rec[i]['x'])
+    #         r.append(rec[i]['y'])
+    #     return r
+
+    # def get_obstacle_cells(self):
+    #     r = []
+    #     rec = self.controller.get_obstacle_cells()
+    #     r.append(rec.__len__())
+    #     for i in range(rec.__len__()):
+    #         r.append(rec[i]['x'])
+    #         r.append(rec[i]['y'])
+    #     return r
+
+    # def get_projectiles(self):
+    #     r = []
+    #     rec = self.controller.get_projectiles()
+    #     r.append(rec.__len__())
+    #     for i in range(rec.__len__()):
+    #         r.append(rec[i]['x'])
+    #         r.append(rec[i]['y'])
+    #     return r
 
 
     def do_move(self, x, y):
