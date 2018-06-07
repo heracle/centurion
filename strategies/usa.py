3fd9570bf5c15e89bc2d35f2f9823e9829724765
saintandy <vladtarniceru@gmail.com>
2018-06-08 00:33:29 +0300

    [add] usa.py bosslang

--- strategies/usa.py
+++ strategies/usa.py
@@ -1,70 +1,37 @@
+import sys
+sys.path.insert(0, '../src')
+from bosslangapi import BosslangApi
+sys.path.insert(0, './bosslang')
+sys.path.insert(0, './bosslang/lib')
+from stdmethods import Injector
+from exe import runsource
+
 def strategy(controller):
-    if controller.round == 15:
-        controller.move({'x':7,'y': 3})
-    if controller.round % 4 == 0:
-        controller.shot({'x': 1, 'y': 2})
-    if controller.round % 4 == 2:
-        controller.shot({'x': 1, 'y': 3})
-
-    if controller.round % 4 == 1:
-        controller.move({'x':8, 'y':3})
-    if controller.round % 4 == 3:
-        controller.move({'x':8, 'y':2})
-    
-    # import sys
-    # sys.path.insert(0, '../src')
-    # from bosslangapi import BosslangApi
-    # sys.path.insert(0, './bosslang')
-    # sys.path.insert(0, './bosslang/lib')
-    # from stdmethods import Injector
-    # from exe import runsource
-
-    # Injector.bosslang_injected_methods = BosslangApi(controller)
-    # runsource("""
-    # program NONAME00;
-    # var round : integer;
-    # begin
-    #     round := CALL(getRound);
-    #     IF round = 15 then
-    #     begin
-    #         CALL(move, 7, 3);
-    #     end;
-
-    #     if round % 4 = 0 then
-    #         CALL(shot, 1, 2);
-    #     end;
-
-    #     if round % 4 = 2 then
-    #         CALL(shot, 1, 3);
-    #     end;
-
-    #     if round % 4 = 1 then
-    #         CALL(move, 8, 3);
-    #     end;
-
-    #     if round % 4 = 3 then
-    #         CALL(move, 8, 2);
-    #     end;
-    # """)  
-
-
-    # if round == 15:
-    #     controller.move({'x':7,'y': 3})
-
-    # if round % 4 == 0:
-    #     controller.shot({'x': 1, 'y': 2})
-    # if round % 4 == 2:
-    #     controller.shot({'x': 1, 'y': 3})
-
-    # if round % 4 == 1:
-    #     controller.move({'x':8, 'y':3})
-    # if round % 4 == 3:
-    #     controller.move({'x':8, 'y':2})
-
-
-    # sys.path.insert(0, '../src')
-    # from bosslangapi import BosslangApi
-    # sys.path.insert(0, './bosslang')
-    # sys.path.insert(0, './bosslang/lib')
-    # from stdmethods import Injector
-    # from exe import runsource
+    Injector.bosslang_injected_methods = BosslangApi(controller)
+    runsource("""
+    program NONAME00;
+    var round : integer;
+    begin
+        round := CALL(getRound);
+        IF round = 15 then
+        begin
+            CALL(move, 7, 3);
+        end;
+        if round - round div 4 * 4 = 0 then
+        begin
+            CALL(shot, 1, 2);
+        end;
+        if round - round div 4 * 4 = 2 then
+        begin
+            CALL(shot, 1, 3);
+        end;
+        if round - round div 4 * 4 = 1 then
+        begin
+            CALL(move, 8, 3);
+        end;
+        if round - round div 4 * 4 = 3 then
+        begin
+            CALL(move, 8, 2);
+        end;
+    end.
+    """)  
